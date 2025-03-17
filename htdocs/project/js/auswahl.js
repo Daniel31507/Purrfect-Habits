85c7e5f4a4991c596390d3312c835a0817f5c974
David Berghahn <d.berghahn@HTBLALeonding.onmicrosoft.com>
2025-03-17 17:09:52 +0100

    radiobuttons auswahl.php

--- htdocs/project/js/auswahl.js
+++ htdocs/project/js/auswahl.js
@@ -1,26 +1,23 @@
 function getAllHabits() {
     fetch('../api/getHabits.php')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-
-        if (data.code == 200) {
-            let html = "";
-            for (let i = 0; i < data.habits.length; i++) {
-                let habit = data.habits[i];
-
-                html += `
-                <div class="habitBox">
-                    <h1>${habit.icon} ${habit.name}</h1>
-                </div>
-                `;
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            if (data.code == 200) {
+                let html = "";
+                for (let i = 0; i < data.habits.length; i++) {
+                    let habit = data.habits[i];
+                    html += `
+                    <div class="habitBox">
+                        <input type="radio" id="habit_${i}" name="habit_selection" value="${habit.name}">
+                        <label for="habit_${i}">${habit.icon} ${habit.name}</label>
+                    </div>`;
+                }
+                document.getElementById("boxes").innerHTML = html;
             }
-
-            document.getElementById("boxes").innerHTML = html;
-        }
-    })
-    .catch((error) => {
-        console.error('Fetch error:', error);
-    });
+        })
+        .catch((error) => {
+            console.error('Fetch error:', error);
+        });
 }
 getAllHabits();
