15cb0c513d9151e3be0de7267a25f4870107bd06
Fabian <f.joos@HTBLALeonding.onmicrosoft.com>
2025-03-17 15:55:01 +0100

    tipps Uhrzeiten funktion fertig und funktional

--- htdocs/project/js/tipps.js
+++ htdocs/project/js/tipps.js
@@ -1,15 +1,16 @@
 function getCurrentTimeAndDay() {
     const now = new Date();
-    
+
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
 
     const weekdays = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"];
     const weekday = weekdays[now.getDay()];
 
-    return `${weekday}\n${hours}:${minutes} Uhr`;
+    return `${weekday} ${hours}:${minutes} Uhr`; 
 }
 
-console.log(getCurrentTimeAndDay());
 
-document.getElementById('datum').innerHTML = getCurrentTimeAndDay();
+setInterval(function() {
+    document.getElementById('datum').innerHTML = getCurrentTimeAndDay();
+}, 1000);
