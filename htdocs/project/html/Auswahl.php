55277746a53cb12f996e27bfaf1ff56d74fd13fe
David Berghahn <d.berghahn@HTBLALeonding.onmicrosoft.com>
2025-03-17 16:34:56 +0100

    Auswahl.php Boxen

--- htdocs/project/html/Auswahl.php
+++ htdocs/project/html/Auswahl.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Habits| Purrfect Habits</title>
+    <title>Habits | Purrfect Habits</title>
     <?php
         echo '<link    href="../css/auswahl.css'   . '?' . time() . '" rel="stylesheet">';
+        echo '<script  src="../js/auswahl.js'    . '?' . time() . '" defer></script>';
+
     ?>
 </head>
 <body>
-    <h1>Habits</h1>
+
+    <div id="window">
+    <div id="scrollbar1">
+    <div id="boxes"></div>
+    </div>
+    </div>
 </body>
 </html>
