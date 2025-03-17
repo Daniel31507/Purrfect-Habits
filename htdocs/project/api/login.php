019154ee9de47c2a2cab281b1d971425ae5915ac
Daniel Riemer <d.riemer@htblaleonding.onmicrosoft.com>
2025-03-17 12:20:36 +0100

    Login funktioniert

---
+++ htdocs/project/api/login.php
@@ -0,0 +1,28 @@
+<?php
+
+$answer = array(
+    "code" => 404,
+    "message" => "Not logged in"
+);
+
+if (isset($_POST["user"]) && isset($_POST["password"])) {
+    $user = $_POST["user"];
+    $password = $_POST["password"];
+
+    $data = file_get_contents("../data/users.json");
+    $users = json_decode($data, true);
+
+    foreach ($users['users'] as $currentUser) {
+        if ($currentUser['name'] == $user && $currentUser['pwd'] == $password) {
+            $answer = array(
+                "code" => 200,
+                "message" => "Logged in successfully"
+            );
+            break;
+        }
+    }
+}
+
+echo json_encode($answer);
+
+?>
--- htdocs/project/js/login.js
+++ htdocs/project/js/login.js
@@ -12,7 +12,7 @@ function start(id) {
         
         
             <div id="options">
-                <div id="opt1">
+                <div id="opt1" onclick="login()">
                 <p>Login</p>
                 </div>
 
@@ -70,3 +70,39 @@ function returnClicked() {
     document.getElementById('logregBox').style.height = '62.5vh';
     document.getElementById('logregBox').innerHTML = html;
 }
+
+
+
+function login() {
+        let user = document.querySelector('#name').value;
+        let pw = document.querySelector('#pwd').value;
+    
+        if (user != '' && pw != '') {
+            let formData = new FormData();
+            formData.append('user', user);
+            formData.append('password', pw);
+    
+            let fetch_URL = '../api/login.php';
+            let fetch_CONFIG = {
+                method: "POST",
+                body: formData
+            }
+    
+            fetch(fetch_URL, fetch_CONFIG)
+                .then((response) => { return response.json(); })
+                .then((data) => {
+                    console.log(data);
+    
+                    if (data.code == 200) {
+                       window.location.href = '../html/Main.html'
+                    } else {
+                        alert("Falsche Anmeldedaten");
+                    }
+                })
+                .catch((error) => {
+                    console.error('Fetch error:', error);
+                });
+        } else {
+            console.log('Fehler');
+        }
+    }
