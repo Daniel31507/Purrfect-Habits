83ffd9d8573cc8ce08c159515baabfcd07a34cfa
NiiloRieser <n.rieser@HTBLALeonding@onmicrosoft.com>
2025-03-24 12:28:56 +0100

    Mappe Button

--- htdocs/project/html/Mappe.php
+++ htdocs/project/html/Mappe.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mappe | Purrfect Habits</title>
     <link rel="stylesheet" href="../css/mappe.css">
 </head>
+
 <body>
     <div id="folder">
         <img src="../img/Mappe.png" alt="Mappe" id="folderImg">
     </div>
+    <a href="./Hauptseite.php" id="returnLink">
+        <div id="returnButton">
+            Zurück
+        </div>
+    </a>
+
 </body>
+
 </html>
