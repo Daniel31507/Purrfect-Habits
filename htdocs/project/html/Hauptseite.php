5b9bcb085450d01be959bbb8b50d4c2a8a92246c
Daniel Riemer <d.riemer@htblaleonding.onmicrosoft.com>
2025-03-24 12:44:02 +0100

    update

--- htdocs/project/html/Hauptseite.php
+++ htdocs/project/html/Hauptseite.php
@@ -22,14 +22,20 @@
         <img src="../img/icons/stress.png">
     </div>
 
+    <div class="clock">
+        <span style="--i:1"><b>1</b></span>
+        <span style="--i:2"><b>2</b></span>
+        <span style="--i:3"><b>3</b></span>
+        <span style="--i:4"><b>4</b></span>
+        <span style="--i:5"><b>5</b></span>
+        <span style="--i:6"><b>6</b></span>
+        <span style="--i:7"><b>7</b></span>
+        <span style="--i:8"><b>8</b></span>
+        <span style="--i:9"><b>9</b></span>
+        <span style="--i:10"><b>10</b></span>
+        <span style="--i:11"><b>11</b></span>
+        <span style="--i:12"><b>12</b></span>
 
-    <div class="clock-container">
-        <div class="clock">
-            <div class="hour" id="hour"></div>
-            <div class="minute" id="minute"></div>
-            <div class="second" id="second"></div>
-        </div>
-        <div class="weekday" id="weekday"></div>
     </div>
 
     <div id="arrowButton" onclick="arrowButton()">&#8680</div>
