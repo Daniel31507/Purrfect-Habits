51666d5d6020651d8e3e7efb475b96da8af60efe
NiiloRieser <n.rieser@HTBLALeonding@onmicrosoft.com>
2025-04-22 12:06:16 +0200

    Funktion Notiz hinzufügen teilweise umgesetzt

--- htdocs/project/html/Mappe.php
+++ htdocs/project/html/Mappe.php
@@ -6,33 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mappe | Purrfect Habits</title>
     <?php
-        echo '<link    href="../css/mappe.css'   . '?' . time() . '" rel="stylesheet">';
-        echo '<script  src="../js/mappe.js'    . '?' . time() . '" defer></script>';
+    echo '<link    href="../css/mappe.css'   . '?' . time() . '" rel="stylesheet">';
+    echo '<script  src="../js/mappe.js'    . '?' . time() . '" defer></script>';
     ?>
 </head>
 
 <body>
     <div id="folder">
-        <div id="leftPage">
+        <div id="pages">
+            <div id="leftPage">
 
-        </div>
-        <div id="rightPage">
+            </div>
+            <div id="rightPage">
 
+            </div>
         </div>
+
         <img src="../img/Mappe.png" alt="Mappe" id="folderImg">
     </div>
-    
+
     <div id="bottomLine">
-            <a href="./Hauptseite.php" id="returnLink">
-        <div id="returnButton">
-            Zurück
+        <a href="./Hauptseite.php" id="returnLink">
+            <div id="returnButton">
+                Zurück
+            </div>
+        </a>
+        <div id="btnAddNote" onclick="addNotePopUp()">
+            +
         </div>
-    </a>
-    <div id="btnAddNote" onclick="addNote()">
-        +
-    </div>
     </div>
 
+
+    <div id="popUpBack">
+        <div id="addNotePopUp">
+            <div id="btnExitPopUp" onclick="closePopUp()">
+                X
+            </div>
+            <h2>Notiz erstellen</h2>
+            <input type="text" id="noteInput">
+            <div id="confirmNote" onclick="addNote()">
+                Fertig
+            </div>
+        </div>
+    </div>
 
 </body>
 
--- htdocs/project/js/mappe.js
+++ htdocs/project/js/mappe.js
@@ -1,10 +1,25 @@
-function addNote(){
+function addNotePopUp() {
+    document.getElementById("popUpBack").style.display = "flex";
+}
 
+function closePopUp() {
+    document.getElementById("popUpBack").style.display = "none";
 
-    // printAllNotes();
+}
+
+function addNote() {
+    let note = document.getElementById("noteInput").value;
+
+    if (note == "") {
+        alert("Falscher Input")
+    } else {
+
+    
+        // printAllNotes();
+    }
 }
 
 
-function printAllNotes(){
+    function printAllNotes() {
 
-}
+    }
